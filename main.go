09e4a73eafd508ea31a4938183b5c01f7706e9ae
Soufiane-Boukhar <boukhar.soufiane.coding@gmail.com>
2024-07-15 11:21:41 +0100

    update work

--- main.go
+++ main.go
@@ -1,62 +1,16 @@
 package main
 
 import (
-    "database/sql"
-    "encoding/json"
     "fmt"
-    "log"
-
-    _ "github.com/go-sql-driver/mysql"
+    "net/http"
 )
 
-type Contact struct {
-    ID       int    `json:"id"`
-    Email    string `json:"email"`
-    Message  string `json:"message"`
-    Subject  string `json:"subject"`
-    FullName string `json:"full_name"`
-    Tel      string `json:"tel"`
+func main() {
+    http.HandleFunc("/", handler)
+    fmt.Println("Server is running on localhost:3000")
+    http.ListenAndServe(":3000", nil)
 }
 
-func main() {
-    dsn := "avnadmin:AVNS_wWoRjEZRmFF5NgjGCcY@tcp(mysql-1fb82b3b-boukhar-d756.e.aivencloud.com:20744)/defaultdb?tls=false"
-
-    db, err := sql.Open("mysql", dsn)
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer db.Close()
-
-    err = db.Ping()
-    if err != nil {
-        log.Fatal(err)
-    }
-    fmt.Println("Connected to the database successfully!")
-
-    rows, err := db.Query("SELECT * FROM contacts")
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer rows.Close()
-
-    var contacts []Contact
-
-    for rows.Next() {
-        var c Contact
-        if err := rows.Scan(&c.ID, &c.Email, &c.Message, &c.Subject, &c.FullName, &c.Tel); err != nil {
-            log.Fatal(err)
-        }
-        contacts = append(contacts, c)
-    }
-
-    if err := rows.Err(); err != nil {
-        log.Fatal(err)
-    }
-
-    jsonData, err := json.Marshal(contacts)
-    if err != nil {
-        log.Fatal(err)
-    }
-
-    fmt.Println(string(jsonData))
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello, World!")
 }
