4a31734c2f97598c81d61b69162ee5834f5b1165
Soufiane-Boukhar <boukhar.soufiane.coding@gmail.com>
2024-07-15 11:26:39 +0100

    update work

--- main.go
+++ main.go
@@ -5,12 +5,12 @@ import (
     "net/http"
 )
 
-func main() {
-    http.HandleFunc("/", handler)
-    fmt.Println("Server is running on localhost:3000")
-    http.ListenAndServe(":3000", nil)
+func Handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello, World!")
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello, World!")
+func main() {
+    http.HandleFunc("/", Handler)
+    fmt.Println("Server is running on localhost:3000")
+    http.ListenAndServe(":3000", nil)
 }
