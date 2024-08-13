2b8f33dd01225c63daaa7d37a5d1a6c965795cc0
Soufiane-Boukhar <boukhar.soufiane.coding@gmail.com>
2024-08-13 11:50:50 +0100

    update work

--- api/index.go
+++ api/index.go
@@ -62,7 +62,6 @@ type Payment struct {
 	Status        string    `json:"status"`
 }
 
-const AllowedOrigin = "*"
 
 func getDBConnection() (*sql.DB, error) {
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s", dbUser, dbPassword, dbHost, dbPort, dbName)
@@ -77,21 +76,12 @@ func getDBConnection() (*sql.DB, error) {
 }
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	origin := r.Header.Get("Origin")
-	if origin != AllowedOrigin && origin != "" {
-		http.Error(w, "Origin not allowed", http.StatusForbidden)
-		return
-	}
-
-	w.Header().Set("Access-Control-Allow-Origin", AllowedOrigin)
-	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-
+	
 	if r.Method == http.MethodOptions {
+		// If you still want to handle OPTIONS requests, just return OK
 		w.WriteHeader(http.StatusOK)
 		return
 	}
-
 	
 	switch r.URL.Path {
 	case "/":
